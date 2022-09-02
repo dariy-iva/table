5761700f114227c527968131b3cdeffc07c13462
Дарья Иванова <dashann@mail.ru>
2022-09-02 13:16:02 +0300

    refactor/add DeleteItemButton component

---
+++ src/components/DeleteItemButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <button type="button" @click="onDelete(item)"
+          class="delete-button" :title="title">x
+  </button>
+</template>
+
+<script>
+export default {
+  name: "DeleteItemButton",
+  props: {
+    onDelete: Function,
+    item: Object,
+    title: String,
+  },
+}
+</script>
+
+<style>
+.delete-button {
+  border: none;
+  background: none;
+  font-weight: 700;
+  color: var(--gray-color);
+  transition: all .3s;
+}
+
+.delete-button:hover {
+  color: var(--red-color);
+  transform: scale(1.4);
+}
+</style>
